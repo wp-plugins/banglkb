ce4fed3ad4f8a606559dfb24639719254bdbcb75
lavluda <lavluda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-15 12:21:06 +0000

    version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/banglkb/trunk@135367 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bangla.plugin.php
@@ -0,0 +1,181 @@
+<?
+/*
+Plugin Name: BanglKB
+Plugin URI: http://ekushey.org/?page/web_input_manager
+Description: Allowes you to write Unijoy and Phonetic Bangla in New Post Section & also in comment section. This plugin is first developed by Hasin Hayder (http://hasinhayder.net). And later S. M. Ibrahim (lavlu) (http://www.lavluda.com) added support on comments. In the version 2.0 we added support of Probhat keyboard layout. BanglKB  is released from ekushey (http://ekushey.org)
+Author: S. M. Ibrahim Lavlu
+Version: 2.1
+Author URI: http://www.lavluda.com
+*/
+
+add_action('admin_footer', 'wp_banglakb');
+function wp_banglakb(){
+
+		echo <<<EOT
+		 		<script type="text/javascript" src="../wp-content/plugins/banglakb/unijoy.js"></script>
+		 		<script type="text/javascript" src="../wp-content/plugins/banglakb/phoneticunicode.js"></script>
+		 		<script type="text/javascript" src="../wp-content/plugins/banglakb/probhatunicode.js"></script>
+				<script type="text/javascript">
+					<!--
+						if(swiToolbar = document.getElementById("ed_toolbar")){
+							var swiLength, swiBut, swiStart, swiEnd;
+
+							swiLength = edButtons.length;
+							edButtons[swiLength] = new edButton('ed_swiUnijoy','Unijoy','', '','');
+							var swiBut = swiToolbar.lastChild;
+							while (swiBut.nodeType != 1){
+								swiBut = swiBut.previousSibling;
+							}
+							swiBut = swiBut.cloneNode(true);
+							swiToolbar.appendChild(swiBut);
+							swiBut.value = 'UniJoy';
+							swiBut.title = 'UniJoy';
+							swiBut.onclick = enableUnijoy;
+							swiBut.id = "ed_wpunijoy";
+							
+							swiLength = edButtons.length;
+							edButtons[swiLength] = new edButton('ed_swiPhonetic','Phonetic','', '','');
+							var swiBut = swiToolbar.lastChild;
+							while (swiBut.nodeType != 1){
+								swiBut = swiBut.previousSibling;
+							}
+							swiBut = swiBut.cloneNode(true);
+							swiToolbar.appendChild(swiBut);
+							swiBut.value = 'Phonetic';
+							swiBut.title = 'Phonetic';
+							swiBut.onclick = enablePhonetic;
+							swiBut.id = "ed_wpphonetic";
+							
+							
+							swiLength = edButtons.length;
+							edButtons[swiLength] = new edButton('ed_swiProbhat','Probhat','', '','');
+							var swiBut = swiToolbar.lastChild;
+							while (swiBut.nodeType != 1){
+								swiBut = swiBut.previousSibling;
+							}
+							swiBut = swiBut.cloneNode(true);
+							swiToolbar.appendChild(swiBut);
+							swiBut.value = 'Probhat';
+							swiBut.title = 'Probhat';
+							swiBut.onclick = enableProbhat;
+							swiBut.id = "ed_wpprobhat";
+							
+							swiLength = edButtons.length;
+							edButtons[swiLength] = new edButton('ed_swiEnglish','English','', '','');
+							var swiBut = swiToolbar.lastChild;
+							while (swiBut.nodeType != 1){
+								swiBut = swiBut.previousSibling;
+							}
+							swiBut = swiBut.cloneNode(true);
+							swiToolbar.appendChild(swiBut);
+							swiBut.value = 'English';
+							swiBut.title = 'English';
+							swiBut.onclick = enableEnglish;
+							swiBut.id = "ed_wpenglish";
+
+						}
+
+					//-->
+	function enableUnijoy() {
+	makeUnijoyEditor('content');
+	makeUnijoyEditor('title');
+	document.getElementById('ed_wpunijoy').style.color="#222EEE";
+	document.getElementById('ed_wpprobhat').style.color="#000";
+	document.getElementById('ed_wpenglish').style.color="#000";
+	document.getElementById('ed_wpphonetic').style.color="#000";
+}
+
+	function enablePhonetic() {
+	makeUniPhoneticEditor('content');
+	makeUniPhoneticEditor('title');
+	document.getElementById('ed_wpunijoy').style.color="#000";
+	document.getElementById('ed_wpenglish').style.color="#000";
+	document.getElementById('ed_wpprobhat').style.color="#000";
+	document.getElementById('ed_wpphonetic').style.color="#222EEE";
+}
+
+	function enableProbhat() {
+	makeProbhatEditor('content');
+	makeProbhatEditor('title');
+	document.getElementById('ed_wpprobhat').style.color="#222EEE";
+	document.getElementById('ed_wpunijoy').style.color="#000";
+	document.getElementById('ed_wpenglish').style.color="#000";
+	document.getElementById('ed_wpphonetic').style.color="#000";
+}
+
+	function enableEnglish() {
+	switched=!switched;
+	if (switched)
+	document.getElementById('ed_wpenglish').value="Bangla";
+	else
+	document.getElementById('ed_wpenglish').value="English";
+	document.getElementById('ed_wpprobhat').style.color="#000";
+	document.getElementById('ed_wpunijoy').style.color="#000";
+	document.getElementById('ed_wpenglish').style.color="#222EEE";
+	document.getElementById('ed_wpphonetic').style.color="#000";
+}
+				</script>
+EOT;
+}
+
+add_action('bangla_kb', 'wp_banglakb_comments');
+
+function wp_banglakb_comments(){
+
+		echo <<<EOT
+		 		<script type='text/javascript' src='<?php echo get_option('siteurl'); ?>/wp-content/plugins/banglakb/unijoy.js'></script>
+		 		<script type='text/javascript' src='<?php echo get_option('siteurl'); ?>/wp-content/plugins/banglakb/phoneticunicode.js'></script>
+		 		<script type='text/javascript' src='<?php echo get_option('siteurl'); ?>/wp-content/plugins/banglakb/probhatunicode.js'></script>
+				<script type='text/javascript'>
+								
+				function enableUnijoy() {
+				makeUnijoyEditor('comment');
+				makeUnijoyEditor('author');
+				document.getElementById('unijoy').style.color="#222EEE";
+				document.getElementById('phonetic').style.color="#000";
+				document.getElementById('english').style.color="#000";
+				document.getElementById('probhat').style.color="#000";
+			}
+			
+				function enablePhonetic() {
+				makePhoneticEditor('comment');
+				makePhoneticEditor('author');
+				document.getElementById('unijoy').style.color="#000";
+				document.getElementById('english').style.color="#000";
+				document.getElementById('phonetic').style.color="#222EEE";
+				document.getElementById('probhat').style.color="#000";
+			}
+				
+				function enableProbhat() {
+				makeProbhatEditor('comment');
+				makeProbhatEditor('author');
+				document.getElementById('unijoy').style.color="#000";
+				document.getElementById('english').style.color="#000";
+				document.getElementById('probhat').style.color="#222EEE";
+				document.getElementById('phonetic').style.color="#000";
+			}
+			
+				function enableEnglish() {
+				switched=!switched;
+				if (switched)
+				document.getElementById('english').value="Bangla";
+				else
+				document.getElementById('english').value="English";
+				document.getElementById('probhat').style.color="#000";
+				document.getElementById('unijoy').style.color="#000";
+				document.getElementById('english').style.color="#222EEE";
+				document.getElementById('phonetic').style.color="#000";
+			}
+				
+				</script>
+
+
+<input type='button' value='unijoy' id='unijoy'onclick='enableUnijoy();'></input>
+<input type='button' value='phonetic' id='phonetic' onclick='enablePhonetic();'></input>
+<input type='button' value='probhat' id='probhat' onclick='enableProbhat();'></input>
+<input type='button' value='english' id='english' onclick='enableEnglish();'></input>
+
+EOT;
+				
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,54 @@
+=== Plugin Name ===
+Contributors: lavluda
+Donate link: http://www.ekushey.org/
+Tags: keyboard, bangla
+Requires at least: 1.5
+Tested up to: 2.8.1
+Stable tag: 2.0
+
+Bangla Typing Scripts for wordpress. This Java Script based add-ons will let your visitors type in Bangla without using any 3rd party tool or keyboard manager along with in your "new post page" you will get option to write post in bangla.
+
+== Description ==
+
+Bangla Typing Scripts for wordpress. This Java Script based add-ons will let your visitors type in Bangla without using any 3rd party tool or keyboard manager along with in your "new post page" you will get option to write post in bangla. These add-ons are very flexible and easy to install to any site and 100% workable with Mozilla FireFox (1.0 or higher), Apple Safari, Microsoft Internet Explorer (5 or higher) and any Gecko engine based web browser.
+
+Currently 3 layouts have been developed for WordPress and they are in mature status.
+
+== Installation ==
+1. download the zip file
+
+2. After extracting the wp-banglakb.zip file you'll find a folder called banglakb. Upload the banglakb folder to your WordPress' /wp-content/plugins/ folder. Again double check that you have bangla.plugin.php, phoneticunicode.js, probhatunicode.js & unijoy.js files inside /wp-content/plugins/banglakb folder. Else this plugin won't work.
+
+3. Log-in to your WordPress Control Panel.
+
+4. Go to Plugins option, You'll see BanglKB plugin name there (as given on the above image). Click on Activate link under the Action column. When the plugin gets activated the whole row gets Green.
+
+5. Now go to Users link from options column. Under Users option, click on Your Profile and make sure the Use the visual editor when writing option is unchecked.
+
+6. Now go to Write, You can see 4 new buttons in the formatting bar. They are: UniJoy, Phonetic, probhat & English.
+
+7. Click on any of this button to activate the layout. to back to english click english.
+
+* If you want to let your visitors make Bangla comment on your blog without using any 3rd party software, just add
+
+<?php wp_banglakb_comments(); ?>
+
+After "comment" textarea.
+
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Changelog ==
+
+= 2.0 =
+* keyboard js are updated for bug fixes.
+* probhat layout added.
+
+= 1.5 =
+* now this plugin will also work in comment form
+
+= 1.0 =
+* first release, unijoy, phonetic and english layout are working on post writepanel.
+
